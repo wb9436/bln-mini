724fae018c5111b090e71ff1b96cc24de75540b4
wb9436 <1016065991@qq.com>
2019-02-25 18:07:54 +0800

    ...

--- src/app.js
+++ src/app.js
@@ -106,6 +106,34 @@ class App extends Component {
     } catch (e) {
       console.log(e)
     }
+
+    if (Taro.getEnv() === Taro.ENV_TYPE.WEAPP && Taro.canIUse('getUpdateManager')) {
+      const updateManager = Taro.getUpdateManager()
+      updateManager.onCheckForUpdate(function (res) {
+        // 请求完新版本信息的回调
+        if (res.hasUpdate) {
+          updateManager.onUpdateReady(function () {
+            Taro.showModal({
+              title: '更新提示',
+              content: '新版本已经准备好，是否重启应用？',
+              confirmColor: '#EE735D',
+              success: function (result) {
+                if (result.confirm) {
+                  updateManager.applyUpdate() //新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+                }
+              }
+            })
+          })
+          updateManager.onUpdateFailed(function () {
+            // 新的版本下载失败
+            Taro.showModal({
+              title: '已经有新版本了哟~',
+              content: '新版本已经上线啦~，请您删除当前小程序，重新搜索打开哟~'
+            })
+          })
+        }
+      })
+    }
   }
 
   // 在 App 类中的 render() 函数没有实际作用
