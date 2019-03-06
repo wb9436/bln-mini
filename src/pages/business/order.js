4142f69aa52c0d42bba8aed2713c749609a54e73
wb9436 <1016065991@qq.com>
2019-03-06 18:46:54 +0800

    ...

--- src/pages/business/order.js
+++ src/pages/business/order.js
@@ -2,6 +2,7 @@ import Taro, {Component} from '@tarojs/taro'
 import {View, ScrollView, Image} from '@tarojs/components'
 import './order.scss'
 
+import LoadAll from '../../components/LoadAll/index'
 import * as Utils from '../../utils/utils'
 import * as Api from '../../store/business/service'
 
@@ -38,89 +39,58 @@ class BusinessOrder extends Component {
   }
 
   render() {
+    const {activityList, loadAll} = this.state
+    const actContent = activityList.map((item, index) => {
+      return <View key={index} className='act-order-item'>
+        <View className='order-content'>
+          <View className='order-act'>
+            <Image className='order-act-icon' src={item.iconUrl} mode='scaleToFill' />
+            <View className='order-act-content'>
+              <View className='order-act-title'>{`【${item.title}】${item.subTitle}`}</View>
+              <View className='order-act-state'>
+                <View className='act-state'>
+                  <Image className='act-icon' src={item.state == 1 ? doing : complete} mode='widthFix' />
+                  <View className={item.state == 1 ? 'act-state-desc' : 'act-state-desc font-disabled'}>
+                    {item.state == 1 ? '进行中' : '已完成'}
+                  </View>
+                </View>
+                <View className='act-money'>{`￥${Utils.formatPricePoint(item.receivedMoney)}`}</View>
+              </View>
+            </View>
+          </View>
+          <View className='order-act-data'>
+            <View className='data-item act-hit'>
+              <View className='data-item-title'>点击量：</View>
+              <View className='data-info'>{item.hits}</View>
+            </View>
+            <View className='data-item act-share'>
+              <View className='data-item-title'>转发数：</View>
+              <View className='data-info'>{item.share}</View>
+            </View>
+            <View className='data-item act-effect'>
+              <View className='data-item-title'>有效阅读数：</View>
+              <View className='data-info'>{`${item.progress}/${item.plan}`}</View>
+            </View>
+          </View>
+          <View className='order-act-btn'>
+            <View className='order-btn-item order-data-stat'>数据统计</View>
+            <View className={item.receivedMoney > 0 ? 'order-btn-item order-invoice' : 'order-btn-item order-invoice bg-disabled'}>
+              申请发票
+            </View>
+          </View>
+        </View>
+      </View>
+    })
+
     return (
       <View className='business-act-page'>
         <ScrollView className='scroll-container'
           scrollY
           scrollWithAnimation
         >
+          {actContent}
 
-          <View className='act-order-item'>
-            <View className='order-content'>
-              <View className='order-act'>
-                <Image className='order-act-icon' src='http://upload.viplark.com/bln/activity_icon/275_408752.jpg' mode='scaleToFill' />
-                <View className='order-act-content'>
-                  <View className='order-act-title'>【官方】百灵鸟，一个了解城市生活的窗口</View>
-                  <View className='order-act-state'>
-                    <View className='act-state'>
-                      <Image className='act-icon' src={doing} mode='widthFix' />
-                      <View className='act-state-desc font-disabled'>进行中</View>
-                    </View>
-                    <View className='act-money'>{`￥${Utils.formatPricePoint(0.3)}`}</View>
-                  </View>
-                </View>
-              </View>
-              <View className='order-act-data'>
-                <View className='data-item act-hit'>
-                  <View className='data-item-title'>点击量：</View>
-                  <View className='data-info'>1000000000</View>
-                </View>
-                <View className='data-item act-share'>
-                  <View className='data-item-title'>转发数：</View>
-                  <View className='data-info'>1000000000</View>
-                </View>
-                <View className='data-item act-effect'>
-                  <View className='data-item-title'>有效阅读数：</View>
-                  <View className='data-info'>10000/10000</View>
-                </View>
-              </View>
-
-              <View className='order-act-btn'>
-                <View className='order-btn-item order-data-stat'>数据统计</View>
-                <View className='order-btn-item order-invoice'>申请发票</View>
-              </View>
-
-            </View>
-          </View>
-
-          <View className='act-order-item'>
-            <View className='order-content'>
-              <View className='order-act'>
-                <Image className='order-act-icon' src='http://upload.viplark.com/bln/activity_icon/275_408752.jpg' mode='scaleToFill' />
-                <View className='order-act-content'>
-                  <View className='order-act-title'>【官方】百灵鸟，一个了解城市生活的窗口</View>
-                  <View className='order-act-state'>
-                    <View className='act-state'>
-                      <Image className='act-icon' src={doing} mode='widthFix' />
-                      <View className='act-state-desc font-disabled'>进行中</View>
-                    </View>
-                    <View className='act-money'>{`￥${Utils.formatPricePoint(0.3)}`}</View>
-                  </View>
-                </View>
-              </View>
-              <View className='order-act-data'>
-                <View className='data-item act-hit'>
-                  <View className='data-item-title'>点击量：</View>
-                  <View className='data-info'>1000000000</View>
-                </View>
-                <View className='data-item act-share'>
-                  <View className='data-item-title'>转发数：</View>
-                  <View className='data-info'>1000000000</View>
-                </View>
-                <View className='data-item act-effect'>
-                  <View className='data-item-title'>有效阅读数：</View>
-                  <View className='data-info'>10000/10000</View>
-                </View>
-              </View>
-
-              <View className='order-act-btn'>
-                <View className='order-btn-item order-data-stat'>数据统计</View>
-                <View className='order-btn-item order-invoice'>申请发票</View>
-              </View>
-
-            </View>
-          </View>
-
+          <LoadAll loadAll={loadAll} />
         </ScrollView>
       </View>
     )
