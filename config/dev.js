4784c93e372d32f4a13fd7b10fadfc52bdc7b504
wb9436 <1016065991@qq.com>
2019-03-14 09:09:47 +0800

    ...

--- config/dev.js
+++ config/dev.js
@@ -3,9 +3,9 @@ module.exports = {
     NODE_ENV: '"development"'
   },
   defineConstants: {
-    BASE_API: '"https://test.viplark.com"',
-    PDD_API: '"https://test.viplark.com"',
-    TOPIC_API: '"https://test.viplark.com"',
+    BASE_API: '"https://api.vipsunyou.com"',
+    PDD_API: '"https://ddk.vipsunyou.com"',
+    TOPIC_API: '"https://chat.vipsunyou.com"',
     WX_WEB: '"https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx14f256ba4fe122d3&redirect_uri=http://api.viplark.com/api/user/wxH5Login&response_type=code&scope=snsapi_userinfo&state=a#wechat_redirect5"'
   },
   weapp: {},
